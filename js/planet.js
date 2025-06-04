4ec6e9ba0790cb22c0042194614f9a99de1eb251
Lyle Watkins <85589327+llwatkin@users.noreply.github.com>
2025-06-03 17:12:10 -0700

    Separated terrain code into new class

--- js/planet.js
+++ js/planet.js
@@ -9,9 +9,7 @@ class Planet {
         this.orbitTime = PLANET_ORBIT_TIME; // 2 minute orbit
         this.sunAngle = createVector(1, 0.25, -0.75);
         this.sunAngleXZ = createVector(this.sunAngle.x, this.sunAngle.z);
-
-        this.debugVerts = [];
-        this.createTerrain();
+        this.terrain = new Terrain(this.rad);
 
         // CITY CODE:
         this.numCities = 6; // number of city clusters generated 
@@ -237,132 +235,6 @@ class Planet {
         }
     }
 
-    drawDebugVerts() {
-        push();
-        strokeWeight(10);
-        for (let i = 0; i < this.debugVerts.length; i++) {
-            if (i < 4) stroke(255, 0, 0);
-            else if (i < 8) stroke(0, 255, 0);
-            else stroke(0, 0, 255);
-            point(this.debugVerts[i]);
-        }
-        pop();
-    }
-
-    createTerrain() {
-        // Free any previous terrain to save memory
-        if (this.terrain) this.clearTerrain();
-        this.terrain = new p5.Geometry();
-
-        let y = this.rad / 2;
-        let x = y * GOLDEN_RATIO;
-
-        // Create basic icosahedron vertices
-        this.debugVerts.push(createVector(-x, y, 0));
-        this.debugVerts.push(createVector(x, y, 0));
-        this.debugVerts.push(createVector(x, -y, 0));
-        this.debugVerts.push(createVector(-x, -y, 0));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[0], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[1], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[2], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[3], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[4], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[5], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[6], 90));
-        this.debugVerts.push(vectorRotateXY(this.debugVerts[7], 90));
-
-        // Add debug verts and subdivide faces
-        for (let vert of this.debugVerts) {
-            this.terrain.vertices.push(vert);
-        }
-        for (let face of ICOSPHERE_FACES) {
-            this.subdivide(face, [], 0, TERRAIN_FIDELITY);
-        }
-
-        // Vary the terrain vertices using Perlin noise
-        this.varyTerrain();
-
-        // This makes the lighting work
-        this.terrain.computeNormals(FLAT);
-    }
-
-    varyTerrain() {
-        let noiseScale = 0.2;
-        for (let i = 0; i < this.terrain.vertices.length; i++) {
-            let vert = this.terrain.vertices[i];
-            let variation = map(
-                noise(noiseScale * i),
-                0, 1,
-                vert.mag() - MIN_TERRAIN_MOD,
-                vert.mag() + MAX_TERRAIN_MOD
-            );
-            vert.setMag(variation);
-        }
-    }
-
-    clearTerrain() {
-        freeGeometry(this.terrain);
-    }
-
-    subdivide(face, faces, level, depth) {
-        // Base case
-        if (level == depth) {
-            for (let f of faces) {
-                this.terrain.faces.push(f);
-            }
-            return;
-        }
-
-        // Get face vertices
-        let v1i = face[0];
-        let v2i = face[1];
-        let v3i = face[2];
-        let v1 = this.terrain.vertices[v1i];
-        let v2 = this.terrain.vertices[v2i];
-        let v3 = this.terrain.vertices[v3i];
-
-        // Create new slerped vertices, subdividing original face
-        let v4 = p5.Vector.slerp(v1, v2, 0.5);
-        let v5 = p5.Vector.slerp(v2, v3, 0.5);
-        let v6 = p5.Vector.slerp(v3, v1, 0.5);
-
-        for (let vert of this.terrain.vertices) {
-            if (isRoundedVectorEqual(v4, vert)) v4 = vert;
-            if (isRoundedVectorEqual(v5, vert)) v5 = vert;
-            if (isRoundedVectorEqual(v6, vert)) v6 = vert;
-        }
-
-        let v4i = this.terrain.vertices.indexOf(v4);
-        let v5i = this.terrain.vertices.indexOf(v5);
-        let v6i = this.terrain.vertices.indexOf(v6);
-
-        if (v4i == -1) {
-            this.terrain.vertices.push(v4);
-            v4i = this.terrain.vertices.indexOf(v4);
-        }
-        if (v5i == -1) {
-            this.terrain.vertices.push(v5);
-            v5i = this.terrain.vertices.indexOf(v5);
-        }
-        if (v6i == -1) {
-            this.terrain.vertices.push(v6);
-            v6i = this.terrain.vertices.indexOf(v6);
-        }
-
-        // Save new faces for later
-        let newFaces = [
-            [v1i, v4i, v6i],
-            [v4i, v5i, v6i],
-            [v5i, v3i, v6i],
-            [v4i, v2i, v5i]
-        ];
-
-        // Continue subdividing
-        for (let f of newFaces) {
-            this.subdivide(f, newFaces, level + 1, depth);
-        }
-    }
-
     drawPlanet() {
         push();
         noStroke();
@@ -370,8 +242,7 @@ class Planet {
         directionalLight(color(255), this.sunAngle); // Sun
         fill(20, 50, 200);
         sphere(this.rad);
-        fill(50, 200, 80);
-        model(this.terrain);
+        this.terrain.draw();
         pop();
     }
 
---
+++ js/terrain.js
@@ -0,0 +1,137 @@
+class Terrain {
+	constructor(rad) {
+		this.rad = rad;
+		this.debugVerts = [];
+		this.createTerrain();
+	}
+
+	drawDebugVerts() {
+		push();
+		strokeWeight(10);
+		for (let i = 0; i < this.debugVerts.length; i++) {
+			if (i < 4) stroke(255, 0, 0);
+			else if (i < 8) stroke(0, 255, 0);
+			else stroke(0, 0, 255);
+			point(this.debugVerts[i]);
+		}
+		pop();
+	}
+
+	createTerrain() {
+		// Free any previous terrain to save memory
+		if (this.mesh) this.clearTerrain();
+		this.mesh = new p5.Geometry();
+
+		let y = this.rad / 2;
+		let x = y * GOLDEN_RATIO;
+
+		// Create basic icosahedron vertices
+		this.debugVerts.push(createVector(-x, y, 0));
+		this.debugVerts.push(createVector(x, y, 0));
+		this.debugVerts.push(createVector(x, -y, 0));
+		this.debugVerts.push(createVector(-x, -y, 0));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[0], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[1], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[2], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[3], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[4], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[5], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[6], 90));
+		this.debugVerts.push(vectorRotateXY(this.debugVerts[7], 90));
+
+		// Add debug verts and subdivide faces
+		for (let vert of this.debugVerts) {
+			this.mesh.vertices.push(vert);
+		}
+		for (let face of ICOSPHERE_FACES) {
+			this.subdivide(face, [], 0);
+		}
+
+		// Vary the terrain vertices using Perlin noise
+		this.varyTerrain();
+
+		// This makes the lighting work
+		this.mesh.computeNormals(FLAT);
+	}
+
+	varyTerrain() {
+		let noiseScale = 0.2;
+		for (let i = 0; i < this.mesh.vertices.length; i++) {
+			let vert = this.mesh.vertices[i];
+			let variation = map(
+				noise(noiseScale * i),
+				0, 1,
+				vert.mag() - MIN_TERRAIN_MOD,
+				vert.mag() + MAX_TERRAIN_MOD
+			);
+			vert.setMag(variation);
+		}
+	}
+
+	clearTerrain() {
+		freeGeometry(this.mesh);
+		this.debugVerts = [];
+	}
+
+	subdivide(face, faces, level) {
+		// Base case
+		if (level == TERRAIN_FIDELITY) {
+			for (let f of faces) if (this.mesh.faces.indexOf(f) == -1) this.mesh.faces.push(f);
+			return;
+		}
+
+		// Get face vertices
+		let v1i = face[0];
+		let v2i = face[1];
+		let v3i = face[2];
+		let v1 = this.mesh.vertices[v1i];
+		let v2 = this.mesh.vertices[v2i];
+		let v3 = this.mesh.vertices[v3i];
+
+		// Create new slerped vertices, subdividing original face
+		let v4 = p5.Vector.slerp(v1, v2, 0.5);
+		let v5 = p5.Vector.slerp(v2, v3, 0.5);
+		let v6 = p5.Vector.slerp(v3, v1, 0.5);
+
+		for (let vert of this.mesh.vertices) {
+			if (isRoundedVectorEqual(v4, vert)) v4 = vert;
+			if (isRoundedVectorEqual(v5, vert)) v5 = vert;
+			if (isRoundedVectorEqual(v6, vert)) v6 = vert;
+		}
+
+		let v4i = this.mesh.vertices.indexOf(v4);
+		let v5i = this.mesh.vertices.indexOf(v5);
+		let v6i = this.mesh.vertices.indexOf(v6);
+
+		if (v4i == -1) {
+			this.mesh.vertices.push(v4);
+			v4i = this.mesh.vertices.indexOf(v4);
+		}
+		if (v5i == -1) {
+			this.mesh.vertices.push(v5);
+			v5i = this.mesh.vertices.indexOf(v5);
+		}
+		if (v6i == -1) {
+			this.mesh.vertices.push(v6);
+			v6i = this.mesh.vertices.indexOf(v6);
+		}
+
+		// Save new faces for later
+		let newFaces = [
+			[v1i, v4i, v6i],
+			[v4i, v5i, v6i],
+			[v5i, v3i, v6i],
+			[v4i, v2i, v5i]
+		];
+
+		// Continue subdividing
+		for (let f of newFaces) {
+			this.subdivide(f, newFaces, level + 1);
+		}
+	}
+
+	draw() {
+		fill(50, 200, 80);
+		model(this.mesh);
+	}
+}
